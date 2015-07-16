6efebfeb8437e04a7a559cf2a02e215d4d4c2fd1
sonofeft <ctatsourceforge@gmail.com>
2015-07-15 17:14:51 -0700

    remove fulltoc from config.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
-    'sphinxcontrib.fulltoc'
+    #'sphinxcontrib.fulltoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
